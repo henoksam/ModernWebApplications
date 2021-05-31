df0551203540da31b19b45ccfecc2f186974854c
Henok Samuel Taddese <81705799+henoksam@users.noreply.github.com>
2021-05-30 22:12:40 -0500

    Add files via upload

---
+++ HomeWork_1_Henok/app.js
@@ -0,0 +1,7 @@
+const child_process = require("child_process");
+
+console.log("1: start");
+const newProcess = child_process.spawn("node", ["computation/fibonacci.js"], {
+  stdio: "inherit",
+});
+console.log("2: End");
